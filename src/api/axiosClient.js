1f5a0204801e4f0047d03d4984a36c2fabc88c85
dinhphu1806 <dinhphu.le1806it@gmail.com>
2023-03-15 12:01:25 +0700

    okkok

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  // baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: "https://63de969a3d94d02c0bade940.mockapi.io/api/v1",
 });
 
 export default client;
